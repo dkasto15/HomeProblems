4eab7ce582ce43861ee11da286812d6f5649b9bc
David K <david.kasto@gmail.com>
2018-04-10 17:54:46 +0000

    fds

--- HW1_2.py
+++ HW1_2.py
@@ -3,57 +3,7 @@ import matplotlib.pyplot as plt
 from scipy.linalg import eig
 from random import *
 
-
 def main():
-    ''' Task 1 '''
-    # # # a) # # #
-
-    # # Ensemble average # #
-    N = 100000 # nbr of realizations
-    t = 1
-    x_ens = 0
-    for i in range(N):
-        w = uniform(2, 6) # Uniformly randomized number between 2 and 6
-        if i == 0:
-            w1 = w
-        x_ens += 2 * np.sin(w * t) # Stoch. proc. val. at arb. time point
-    x_ens = 1 / float(N) * x_ens
-    print('1a)', x_ens)
-    print('1a), analytical', 1 / (2 * float(t)) * (np.cos(2 * t) - np.cos(6 * t)))
-
-    # # Time average # #
-    M = 100000 # nbr of timesteps
-    # w = uniform(2, 6) # Uniformly randomized number between 2 and 6
-    t = np.linspace(0,100000,M) # vector with discrete timesteps
-    x_t = 1 / float(M) * np.sum(2 * np.sin(w1 * t))
-    print('1a)', x_t)
-
-    # # ^Ergodic means timeAv=ensemAv, this is not the case here. # #
-
-    # # # b) # # #
-
-    w0 = 1
-    # # Ensemble average # #
-    N = 10000 # nbr of realizations
-    t = 1
-    x_ens = 0
-    for i in range(N):
-        theta = uniform(0, 2 * np.pi) # Uniformly randomized number between 0 and 2*pi
-        if i == 0:
-            theta1 = theta
-        x_ens += 2 * np.sin(w0 * t + theta) # Stoch. proc. val. at arb. time point
-    x_ens = 1 / float(N) * x_ens
-    print('1b)', x_ens)
-
-    # # Time average # #
-    M = 100000 # nbr of timesteps
-    # w = uniform(2, 6) # Uniformly randomized number between 2 and 6
-    t = np.linspace(0,100000,M) # vector with discrete timesteps
-    x_t = 1 / float(M) * np.sum(2 * np.sin(w0 * t + theta1))
-    print('1b)', x_t)
-
-    # # ^Ergodic means timeAv=ensemAv, this is not the case here. # #
-
     ''' Task 2 '''
     # # # 1.a) # # #
     W = [[1/4.0, 0, 1/8.0], [1/2.0, 1/2.0, 3/4.0], [1/4.0, 1/2.0, 1/8.0]]
@@ -70,13 +20,12 @@ def main():
     P_st = eig_vec_r[:, max_ind]
     norm = np.sum(P_st)
     P_st = P_st / norm
-    print(P_st, np.sum(P_st))
-    print(np.dot(W, P_st))
+    print('2.1c)', P_st, np.sum(P_st))
+    print('2.1c)', np.dot(W, P_st))
 
 
     # # # 1.d) # # #
     eig_W_l, eig_vec_l = eig(W, left=True, right=False) # left eigenvalues/-vectors
-    print(eig_W_r)
     for i in range(len(W)):
         if i != max_ind:
             print(eig_W_r[i])
